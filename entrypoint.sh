a9fc4bdbdcc8a5083500f8eb2e71105485ee95b7
Thiago <thiagooliveira1547@gmail.com>
2025-02-14 01:35:21 -0300

    encadeando os comandos

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,28 @@
 # Caminho para um arquivo de flag indicando que o script já rodou
 FLAG_FILE='/root/setup/.setup_done'
 
+echo "=================================================================="
+echo "Iniciando entrypoint.sh..."
+echo "=================================================================="
+
 # Se o arquivo de flag não existir, executa o script e cria o flag
 if [ ! -f "$FLAG_FILE" ]; then
+    echo "Executando setup.sh pela primeira vez..."
     chmod -R +x /root/setup/ && \
     /root/setup/setup.sh
-    touch "$FLAG_FILE"
-
+    if [ $? -eq 0 ]; then
+        echo "setup.sh concluído com sucesso."
+        touch "$FLAG_FILE"
+    else
+        echo "Erro ao executar setup.sh."
+        exit 1
+    fi
 else
     echo "=================================================================="
-    echo "Environment setup concluded succesfully!"
+    echo "Environment setup concluído com sucesso!"
     echo "=================================================================="
 fi
+
+echo "=================================================================="
+echo "Finalizando entrypoint.sh."
+echo "=================================================================="
--- setup.sh
+++ setup.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
+echo "=================================================================="
+echo "Iniciando setup.sh..."
+echo "=================================================================="
+
 # Atualizar e fazer upgrade do sistema
-apt-get upgrade -y
-apt-get update
+echo "Atualizando o sistema..."
+apt-get upgrade -y && \
+apt-get update && \
 
 # Criar um ambiente virtual Python
-apt-get install -y python3-venv
+echo "Criando ambiente virtual..."
+apt-get install -y python3-venv && \
 cd /root/ && python3 -m venv harpia && \
 source harpia/bin/activate
 
 # Instalar a toolchain de desenvolvimento do PX4 para usar o simulador
+echo "Instalando PX4 Autopilot..."
 cd /root/ && git clone https://github.com/PX4/PX4-Autopilot.git --recursive && \
 bash /root/PX4-Autopilot/Tools/setup/ubuntu.sh
 
 # Instalar algumas dependências para ROS
-pip3 install -U empy pyros-genmsg setuptools catkin_pkg lark
-apt-get install -y ros-dev-tools 
+echo "Instalando dependências do ROS..."
+pip3 install -U empy pyros-genmsg setuptools catkin_pkg lark && \
+apt-get install -y ros-dev-tools
 
 # Instalar o XRCE-DDS Agent
+echo "Instalando Micro-XRCE-DDS-Agent..."
 cd /root/ && git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git && \
 cd /root/Micro-XRCE-DDS-Agent && \
 mkdir build && \
@@ -28,6 +37,7 @@ make install && \
 ldconfig /usr/local/lib/
 
 # Clonar os repositórios necessários
+echo "Clonando repositórios..."
 cd /root/estudos_ws/src/ && \
 git clone https://github.com/PX4/px4_msgs.git && \
 git clone https://github.com/PX4/px4_ros_com.git
@@ -35,7 +45,8 @@ git clone https://github.com/PX4/px4_ros_com.git
 # Função para tentar rodar o colcon build
 build_with_retry() {
     while true; do
-        cd /root/estudos_ws/ && colcon build
+        echo "Executando colcon build..."
+        cd /root/estudos_ws/ && colcon build --packages-ignore bringup description interfaces
         if [ $? -eq 0 ]; then  # Verifica se o comando anterior foi bem-sucedido
             echo "colcon build executado com sucesso!"
             break  # Sai do loop se o comando for bem-sucedido
@@ -48,3 +59,7 @@ build_with_retry() {
 
 # Chama a função
 build_with_retry
+
+echo "=================================================================="
+echo "setup.sh concluído com sucesso!"
+echo "=================================================================="
