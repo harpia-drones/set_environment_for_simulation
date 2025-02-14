f7e7e0fd0503c4f970fb156d2168e015f0109e13
Thiago <thiagooliveira1547@gmail.com>
2025-02-14 00:57:21 -0300

    second commit

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Caminho para um arquivo de flag indicando que o script já rodou
+FLAG_FILE='/root/.setup_done'
+
+# Se o arquivo de flag não existir, executa o script e cria o flag
+if [ ! -f "$FLAG_FILE" ]; then
+    chmod -R +x /root/estudos_ws/src/dependencies && \
+    /root/estudos_ws/src/dependencies/setup/setup.sh
+    touch "$FLAG_FILE"
+
+else
+    echo "=================================================================="
+    echo "Environment setup concluded succesfully!"
+    echo "=================================================================="
+fi
+
+# Executa o comando original do container
+exec "$@"
